bd95d4765cabc43056fed6f3e706bbafdfd8f458
sushi200 <pawarsushant531@gmail.com>
2022-10-13 20:48:21 +0530

    initial

--- 3hr_jsProject/main.js
+++
@@ -1,8 +0,0 @@
-let form = document.getElementById("form");
-let list = document.getElementById("expenses");
-
-//form submit event
-form.addEventListener("submit", addExpense);
-
-//delete an event
-list.addEventListener("click", removeItem);
