3f1525c53e75e461ad440f04a9def76bba77e6e5
sushi200 <pawarsushant531@gmail.com>
2022-11-11 15:01:59 +0530

    expenses

--- React/myapp/src/components/ExpenseItem.js
+++ React/myapp/src/components/ExpenseItem.js
@@ -1,4 +1,9 @@
 function ExpenseItem() {
-  return <>Expense Food Rs 10 Petrol Rs 100 Movies Rs 200</>;
+  return (
+    <>
+      Expense Food Rs 10 Petrol Rs 100 Movies Rs 200
+      <div>new addition</div>
+    </>
+  );
 }
 export default ExpenseItem;
