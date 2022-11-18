1340f4a50720bdccc2ce3a9fdb6906636d92d7ec
sushi200 <pawarsushant531@gmail.com>
2022-11-18 13:23:06 +0530

    states_added

--- React/myapp/src/components/NewExpense/ExpenseForm.js
+++ React/myapp/src/components/NewExpense/ExpenseForm.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  //various states
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
   };
   const amountChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredAmount(event.target.value);
   };
   const dateChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredDate(event.target.value);
   };
   return (
     <form action="">
