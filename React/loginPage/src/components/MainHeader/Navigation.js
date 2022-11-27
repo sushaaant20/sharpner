b241038d43aea51e6543e7d812f2a7f44dbbf042
sushi200 <pawarsushant531@gmail.com>
2022-11-27 08:35:05 +0530

    contextApi

--- React/loginPage/src/components/MainHeader/Navigation.js
+++ React/loginPage/src/components/MainHeader/Navigation.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useContext } from "react";
+import AuthContext from "../store/auth-context";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
