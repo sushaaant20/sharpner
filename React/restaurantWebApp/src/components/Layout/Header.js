2df9db01c1875a09e217c9cfa524aee60e626c9b
sushi200 <pawarsushant531@gmail.com>
2022-11-28 10:16:16 +0530

    cart_openClose

--- React/restaurantWebApp/src/components/Layout/Header.js
+++ React/restaurantWebApp/src/components/Layout/Header.js
@@ -2,12 +2,13 @@ import { Fragment } from "react";
 import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
-const Header = () => {
+
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Table full of meal" />
