76d94227ac4bdca6b660064c2f59195e4ea12ca1
sushi200 <pawarsushant531@gmail.com>
2022-12-07 15:43:24 +0530

    task1

---
+++ React/ecom/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <NavBar />
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ React/ecom/src/components/NavBar/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
