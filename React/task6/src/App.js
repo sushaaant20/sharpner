5559a708b30ed2eb964f6f1f049cea5ebf13e093
sushi200 <pawarsushant531@gmail.com>
2022-12-12 14:13:12 +0530

    task8-retrying-function-added

--- React/task6/src/App.js
+++ React/task6/src/App.js
@@ -7,24 +7,46 @@ function App() {
   const [movies, setMovies] = useState([]);
   //loading data state
   const [isLoading, setIsLoading] = useState(false);
+  //Error State
+  const [error, setError] = useState(null);
+  let id;
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying'");
+      }
 
-    const transformedMovies = data.results.map((movieData) => {
-      console.log("data");
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+    console.log("fetchMoviesHandler function runs");
   }
+
+  if (error) {
+    id = setTimeout(fetchMoviesHandler, 5000);
+  }
+  const stopRetryingHandler = () => {
+    console.log("cancel button clicked");
+    clearTimeout(id);
+    setError(null);
+  };
+
   return (
     <React.Fragment>
       <section>
@@ -32,7 +54,14 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <h2>'No Movies'</h2>}
+        {!isLoading && movies.length === 0 && !error && <h2>'No Movies'</h2>}
+        {!isLoading && error && (
+          <p>
+            {error} <br></br>
+            <br></br>
+            <button onClick={stopRetryingHandler}>Cancel </button>
+          </p>
+        )}
         {isLoading && <p>Loading Data...</p>}
       </section>
     </React.Fragment>
